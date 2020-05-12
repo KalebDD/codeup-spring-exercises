e4f32b4c1806b5c1c318022d9cb360285924363e
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-05-12 10:56:51 -0500

    functionality for ex.1 done, working on styling

--- src/main/java/com/codeup/springblogapp/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblogapp/controllers/RollDiceController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Random;
 
 @Controller
 public class RollDiceController {
@@ -17,12 +14,12 @@ public class RollDiceController {
     }
 
     @GetMapping("/roll-dice/{num}")
-    public String test(@PathVariable int num, Model model) {
+    public String checkRollDice(@PathVariable int num, Model model) {
 
         int diceRoll = (int) (1 + (Math.random() * 6));
 
-        String response = "You rolled a: " + num + "<br>"
-                + "The dice rolled a: " + diceRoll + "<br>";
+        String response = "You rolled a: " + num + ". "
+                + "The dice rolled a: " + diceRoll + ". ";
 
         if (num == diceRoll) {
             response += "It's a match!";
