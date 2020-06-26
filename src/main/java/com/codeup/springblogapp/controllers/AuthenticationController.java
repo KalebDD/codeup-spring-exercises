df77293919cb6cb9fb11eee10b757f286a4e3bcc
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-06-26 17:24:02 -0500

    working on refactoring login to modal instead of specific page

--- src/main/java/com/codeup/springblogapp/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/springblogapp/controllers/AuthenticationController.java
@@ -1,18 +1,20 @@
 package com.codeup.springblogapp.controllers;
 
+import com.codeup.springblogapp.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        return "posts/index";
+        return "users/login";
     }
 
     @PostMapping("/login")
-    public String test() {
-        return "posts/index";
+    public String login(@ModelAttribute User user) {
+        return "redirect:/posts";
     }
 }
