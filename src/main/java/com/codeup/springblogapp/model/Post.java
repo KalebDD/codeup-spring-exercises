34e0416bfff2466fc19fb9db773d893dd67fd7c3
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-07-18 19:54:57 -0500

    got creation date time and update date time to generate correctly

--- src/main/java/com/codeup/springblogapp/model/Post.java
+++ src/main/java/com/codeup/springblogapp/model/Post.java
@@ -1,6 +1,10 @@
 package com.codeup.springblogapp.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "posts")
@@ -19,6 +23,16 @@ public class Post {
     @Column
     private String category;
 
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date", updatable = false)
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modify_date")
+    private Date modifyDate;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User owner;
@@ -26,17 +40,19 @@ public class Post {
 
     public Post() { }
 
-    public Post(long id, String title, String category, String description) {
+    public Post(long id, String title, String category, String description, Date createDate) {
         this.id = id;
         this.title = title;
         this.category = category;
         this.description = description;
+        this.createDate = createDate;
     }
 
-    public Post(String title, String category, String description) {
+    public Post(String title, String category, String description, Date createDate) {
         this.title = title;
         this.category = category;
         this.description = description;
+        this.createDate = createDate;
     }
 
     public User getOwner() { return owner; }
@@ -59,6 +75,12 @@ public class Post {
         this.description = description;
     }
 
-    public String getCategory() {return category;}
-    public void setCategory(String category) {this.category = category;}
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public Date getCreateDate() { return createDate; }
+    public void setCreateDate(Date createDate) { this.createDate = createDate; }
+
+    public Date getModifyDate() { return modifyDate; }
+    public void setModifyDate(Date modifyDate) { this.modifyDate = modifyDate; }
 }
